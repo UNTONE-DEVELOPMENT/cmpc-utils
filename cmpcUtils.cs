7a7fdbf552c6cf66406eac33cd0b3f8eecf3497a
BuildTools <33783503+Hubza@users.noreply.github.com>
2021-12-21 16:01:38 +0000

    possibly fix cross-platform .cubey files

--- cmpcUtils.cs
+++ cmpcUtils.cs
@@ -1,13 +1,10 @@
 namespace cmpc
 {
-    using System;
-    using System.IO.Compression;
-
     public static class Utils
     {
         public static void extractFileToFolder(string file, string folder)
         {
-            ZipFile.ExtractToDirectory(file, folder);
+            ZipUtil.Unzip(file, folder);
         }
     }
 }
